5c246acbb23c3fc2a7e0d573f2ef9d8cf63feb73
Anzney <shaswat.singh@anzney.com>
2025-12-12 11:16:06 +0530

    dashboard

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,14 +1,16 @@
 
-import React from 'react';
 
-export default function PublicLayout({
+import React from 'react';
+import DashboardLayout from '@/components/dashboard/Dashboard';
+
+export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <DashboardLayout>
       {children}
-    </>
+    </DashboardLayout>
   );
 }
