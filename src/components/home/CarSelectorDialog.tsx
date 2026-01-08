a5fbaf118d19ede7b376015599e704927577f8ba
iam-awaneesh <aw64an@gmail.com>
2026-01-08 11:38:41 +0530

    remove the one cross button

--- src/components/home/CarSelectorDialog.tsx
+++ src/components/home/CarSelectorDialog.tsx
@@ -125,15 +125,7 @@ export function CarSelectorDialog() {
     return (
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
             <DialogContent className="sm:max-w-3xl overflow-hidden p-0 gap-0 bg-background/95 backdrop-blur-md border-border/40 shadow-2xl">
-                <div className="absolute top-4 right-4 z-50">
-                    <button
-                        onClick={handleClose}
-                        className="rounded-full p-2 bg-background/50 hover:bg-muted transition-colors"
-                    >
-                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 opacity-70"><path d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.1929 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.1929 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                        <span className="sr-only">Close</span>
-                    </button>
-                </div>
+
 
                 <div className="grid md:grid-cols-5 h-[80vh] md:h-[600px]">
                     {/* Left Sidebar / Illustration Area */}
